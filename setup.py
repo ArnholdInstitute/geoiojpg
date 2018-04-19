eebaf33592677248c1c4d13449dff37acf88f59f
Matt Le <ml9951@rit.edu>
2018-04-19 11:26:19 -0400

    change download_url to point to tags

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
+version = "0.5"
+
 setup(
     name='geoiojpg',
     packages=['geoiojpg'],
-    version='0.2',
+    version=version,
     description='A wrapper around GeoIO for non-georeferenced jpg files',
     author='Matt Le',
     author_email='lematt1991@gmail.com',
     url='https://github.com/ArnholdInstitute/geoiojpg',
-    download_url='https://github.com/ArnholdInstitute/geoiojpg/archive/master.zip',
+    download_url='https://github.com/ArnholdInstitute/geoiojpg/archive/{0}.tar.gz'.format(version),
     keywords=['jpg', 'jpeg', 'geoio', 'geospatial'],
     classifiers=[],
     install_requires=['geoio', 'shapely']
